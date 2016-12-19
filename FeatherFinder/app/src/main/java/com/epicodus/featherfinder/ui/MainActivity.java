b57ef3159ba285a051e9190969383d6156d66a70
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-19 11:31:51 -0800

    add profile item and find item to main dropdown menu, and create intent to go to LogBirdActivity when logBirdButton is clicked

--- FeatherFinder/app/src/main/java/com/epicodus/featherfinder/ui/MainActivity.java
+++ FeatherFinder/app/src/main/java/com/epicodus/featherfinder/ui/MainActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.featherfinder.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.logBirdButton) Button mLogBirdButton;
 
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        mLogBirdButton.setOnClickListener(this);
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -64,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLogBirdButton) {
+            Intent intent = new Intent(MainActivity.this, LogBirdActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void logout() {
