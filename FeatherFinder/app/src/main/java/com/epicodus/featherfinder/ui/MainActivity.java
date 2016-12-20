ca156555e514f54affb55de4997ee9039266ebb9
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-19 16:38:22 -0800

    begin to implement geotagging functionality, using LocationManager and LocationListener with NETWORK_PROVIDER

--- FeatherFinder/app/src/main/java/com/epicodus/featherfinder/ui/MainActivity.java
+++ FeatherFinder/app/src/main/java/com/epicodus/featherfinder/ui/MainActivity.java
@@ -1,9 +1,16 @@
 package com.epicodus.featherfinder.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +20,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.epicodus.featherfinder.Manifest;
 import com.epicodus.featherfinder.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,12 +29,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    static final int REQUEST_IMAGE_CAPTURE = 111;
+    private static final int REQUEST_IMAGE_CAPTURE = 111;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
 
     @Bind(R.id.takePhotoImageView) ImageView mTakePhotoImageView;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
+    private String locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +48,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
         mTakePhotoImageView.setOnClickListener(this);
+
+        locationProvider = LocationManager.NETWORK_PROVIDER;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extas){}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
